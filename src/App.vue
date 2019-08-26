653f36ee0978b8c9c0d172811e2c0665caa9f3f1
yong ping Chen <uncochen@hotmail.com>
2019-08-26 23:58:11 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,66 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+                  v-if="questions.length"
+                  :currentQuestion="questions[index]"
+                  :next="next"
+                  :increment="increment"
+          />
+        </b-col>
+
+
+      </b-row>
+    </b-container>
   </div>
 </template>
-
+<script>
+  import Header from './components/Header.vue'
+  import QuestionBox from './components/QuestionBox.vue'
+  export default {
+    name: 'app',
+    components:{
+      Header,
+      QuestionBox
+    },
+    data(){
+      return{
+        questions:[],
+        index:0,
+        numCorrect: 0,
+        numTotal: 0
+      }
+    },
+    methods:{
+      next(){
+        this.index++
+      },
+      increment(isCorrect){
+        if(isCorrect){
+          this.numCorrect++
+        }
+        this.numTotal++
+      }
+    },
+    mounted:function () {
+      fetch('https://opentdb.com/api.php?amount=10&category=10',{
+        method:'get',
+      })
+              .then((response)=>{
+                return response.json()
+              })
+              .then((jsonData)=>{
+                this.questions = jsonData.results
+              })
+    }
+  }
+</script>
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Exam</b></b-nav-item>
+            <b-nav-item disabled>Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
+    <hr>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:[
+            'numCorrect',
+            'numTotal'
+        ]
+
+    }
+</script>
+
+<style scoped>
+
+</style>
